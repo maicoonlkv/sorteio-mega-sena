1394a4434d9f3e20c91c9286cc26be010039a8c6
maicoonlkv <maicon.kvictor@gmail.com>
2024-01-19 18:33:35 -0300

    PRIMEIRA VERSÃO DO PROJETO

---
+++ script.js
@@ -0,0 +1,95 @@
+
+
+function sorteio() {
+
+    const inputNum1 = document.getElementById("firstNumber")
+    const num1 = inputNum1.value
+
+    const min1 = 0
+    const max1 = 25
+
+    const num1STDO = Math.floor(Math.random() * (max1 - min1) + min1);
+    console.log(num1STDO)
+
+    inputNum1.value = num1STDO
+
+    //---------------------------------------
+
+    const inputNum2 = document.getElementById("secondNumber")
+    const num2 = inputNum2.value
+
+    const min2 = 4
+    const max2 = 28
+
+    const num2STDO = Math.floor(Math.random() * (max2 - min2) + min2);
+    console.log(num2STDO)
+
+    inputNum2.value = num2STDO
+
+    //---------------------------------------
+
+    const inputNum3 = document.getElementById("thirdNumber")
+    const num3 = inputNum3.value
+
+    const min3 = 11
+    const max3 = 33
+
+    const num3STDO = Math.floor(Math.random() * (max3 - min3) + min3);
+    console.log(num3STDO)
+
+    inputNum3.value = num3STDO
+
+    //---------------------------------------
+
+    const inputNum4 = document.getElementById("fourthNumber")
+    const num4 = inputNum4.value
+
+    const min4 = 22
+    const max4 = 41
+
+    const num4STDO = Math.floor(Math.random() * (max4 - min4) + min4);
+    console.log(num4STDO)
+
+    inputNum4.value = num4STDO
+
+    //---------------------------------------
+
+    const inputNum5 = document.getElementById("fifthNumber")
+    const num5 = inputNum5.value
+
+    const min5 = 25
+    const max5 = 56
+
+    const num5STDO = Math.floor(Math.random() * (max5 - min5) + min5);
+    console.log(num5STDO)
+
+    inputNum5.value = num5STDO
+
+    //---------------------------------------
+
+    const inputNum6 = document.getElementById("sixthNumber")
+    const num6 = inputNum6.value
+
+    const min6 = 41
+    const max6 = 60
+
+    const num6STDO = Math.floor(Math.random() * (max6 - min6) + min6);
+    console.log(num6STDO)
+
+    inputNum6.value = num6STDO
+
+}
+
+
+
+
+
+
+
+
+/* const num2 = document.getElementById("secondNumber").value
+ const num3 = document.getElementById("thirdNumber").value
+ const num4 = document.getElementById("fourthNumber").value
+ const num5 = document.getElementById("fifthNumber").value
+ const num6 = document.getElementById("sixthNumber").value
+*/
